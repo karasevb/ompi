82aca29429841cd2e2c04567f45dfb90c99ed79e
Boris Karasev <karasev.b@gmail.com>
2018-02-01 20:38:18 +0700

    Fix 5

--- orte/mca/rmaps/base/rmaps_base_support_fns.c
+++ orte/mca/rmaps/base/rmaps_base_support_fns.c
@@ -258,15 +258,14 @@ int orte_rmaps_base_get_target_nodes(opal_list_t *allocated_nodes, orte_std_cntr
             if (NULL == (node = (orte_node_t*)opal_pointer_array_get_item(orte_node_pool, i))) {
                 continue;
             }
-            OPAL_LIST_FOREACH(item, &nodes, orte_node_t) {
-                nptr = (orte_node_t*)item;
+            OPAL_LIST_FOREACH(nptr, &nodes, orte_node_t) {
                 if (0 != strcmp(node->name, nptr->name)) {
                     OPAL_OUTPUT_VERBOSE((10, orte_rmaps_base_framework.framework_output,
                                          "NODE %s DOESNT MATCH NODE %s",
                                          node->name, nptr->name));
                     continue;
                 }
-                opal_list_remove_item(&nodes, item);
+                opal_list_remove_item(&nodes, (opal_list_item_t*)nptr);
                 /* ignore nodes that are marked as do-not-use for this mapping */
                 if (ORTE_NODE_STATE_DO_NOT_USE == node->state) {
                     OPAL_OUTPUT_VERBOSE((10, orte_rmaps_base_framework.framework_output,
@@ -333,8 +332,8 @@ int orte_rmaps_base_get_target_nodes(opal_list_t *allocated_nodes, orte_std_cntr
                     /* reset us back to the end for the next node */
                     nd = (orte_node_t*)opal_list_get_last(allocated_nodes);
                 }
+                OBJ_RELEASE(nptr);
             }
-            OBJ_RELEASE(nptr);
         }
         OBJ_DESTRUCT(&nodes);
         /* now prune for usage and compute total slots */
